10a703ff553f107ce046694651fa6ba4850d9641
Jannishar <mdjannishar654@gmail.com>
2025-08-05 23:19:40 +0530

    Basic Setup

---
+++ init/index.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose"); 
+const initData = require("./data.js"); 
+const Listing = require("../models/listing.js"); 
+
+const MONGO_URL ="mongodb://127.0.0.1:27017/eirastay";
+
+main().then(() =>{
+    console.log("Connected to DB"); 
+}).catch((err) =>{
+    console.log(err); 
+}); 
+
+async function main(){
+    await mongoose.connect(MONGO_URL);
+}
+
+const initDB = async () =>{
+    await Listing.deleteMany({}); 
+    // SInce data has both filename and url , so used it 
+    const simplifiedData = initData.data.map((listing) => {
+    return {
+        ...listing,
+        image: listing.image.url  // extract only the URL as a string
+    };
+});
+
+await Listing.insertMany(simplifiedData);
+    console.log("data was initialised"); 
+}; 
+initDB(); 

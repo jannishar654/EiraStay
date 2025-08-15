761d115d8bd5daa5a465c5aee93457d7ffcbf189
Jannishar <mdjannishar654@gmail.com>
2025-08-15 14:01:43 +0530

    Review validations and Delete Review

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate"); 
 const wrapAsync = require("./utils/wrapAsync.js"); 
 const ExpressError=require("./utils/ExpressError.js");
-const{ listingSchema} = require("./schema.js"); 
+const{ listingSchema,reviewSchema} = require("./schema.js"); 
 const MONGO_URL ="mongodb://127.0.0.1:27017/eirastay";
 const Review = require("./models/review.js"); 
 //console.log("Review model loaded as:", Review);
@@ -34,9 +34,21 @@ app.get("/" ,(req,res) =>{
     res.send(" Hi , I am root"); 
 }); 
 
-// Schema Validation middleware using Joi  
+// Listing Schema Validation middleware using Joi  
 const validateListing =(req,res,next) =>{
     let {error}= listingSchema.validate(req.body); 
+   if(error){
+    let errMsg = error.details.map((el) => el.message).join(","); 
+    throw new ExpressError(400,errMsg); 
+   }else {
+    next(); 
+   }
+
+}
+
+// Review Schema Validation middleware using Joi  
+const validateReview =(req,res,next) =>{
+    let {error}= reviewSchema.validate(req.body); 
    if(error){
     let errMsg = error.details.map((el) => el.message).join(","); 
     throw new ExpressError(400,errMsg); 
@@ -63,7 +75,7 @@ app.get("/listings/new",(req,res) => {
 // Show Route 
 app.get("/listings/:id", async (req,res) => {
     let {id} = req.params; 
-    const listing = await Listing.findById(id); 
+    const listing = await Listing.findById(id).populate("reviews"); 
     res.render("listings/show.ejs", {listing}); 
 })
 // CReate Route 
@@ -110,7 +122,7 @@ app.delete("/listings/:id",wrapAsync(async(req,res) =>{
 })); 
 
 // Reviews //post ruoute 
-app.post("/listings/:id/reviews" ,async(req,res) =>{
+app.post("/listings/:id/reviews" ,validateReview,wrapAsync(async(req,res) =>{
     let listing = await Listing.findById(req.params.id); 
     let newReview = new Review(req.body.review); 
 
@@ -119,9 +131,22 @@ app.post("/listings/:id/reviews" ,async(req,res) =>{
     await newReview.save(); 
     await listing.save(); 
 
-    res.send("New Review Send "); 
+    res.redirect(`/listings/${listing._id}`); 
 
-})
+})); 
+
+// Post route for deleting review 
+app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async (req,res) =>{
+    let {id, reviewId} = req.params; 
+
+    // we will use pull operator to delete that review from reviws array of that listing 
+
+    await Listing.findByIdAndUpdate(id ,{$pull: {reviews:reviewId}}); 
+    await Review.findByIdAndDelete(reviewId); 
+
+    res.redirect(`/listings/${id}`); 
+})) 
+
 
 
 // app.get("/listings/new",(req,res) => {
--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose"); 
 const Schema = mongoose.Schema; 
+const Review = require("./review.js"); 
 
 const listingSchema = new Schema({
     title : {
@@ -24,7 +25,16 @@ const listingSchema = new Schema({
 
     ],
 
-}); 
+});
+
+// POST Mongoose middleware for deleteing reviews also on deleting that review 
+listingSchema.post("findOneAndDelete" ,async (listing) =>{
+    if(listing){
+        await Review.deleteMany({_id :{$in :listing.reviews}}); 
+        
+    }
+    
+})
 
 const Listing = mongoose.model("Listing",listingSchema); 
 module.exports = Listing ; 

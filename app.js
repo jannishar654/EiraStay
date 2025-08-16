a6ce07f9f0a037626d5180128f673d467f4394b7
Jannishar <mdjannishar654@gmail.com>
2025-08-16 16:00:35 +0530

    Different routes files for listings and reviews

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express"); 
 const app = express(); 
 const mongoose = require("mongoose"); 
-const Listing = require("./models/listing.js"); 
 const path = require("path"); 
 const methodOverride = require("method-override"); 
-const ejsMate = require("ejs-mate"); 
-const wrapAsync = require("./utils/wrapAsync.js"); 
+const ejsMate = require("ejs-mate");  
 const ExpressError=require("./utils/ExpressError.js");
-const{ listingSchema,reviewSchema} = require("./schema.js"); 
 const MONGO_URL ="mongodb://127.0.0.1:27017/eirastay";
-const Review = require("./models/review.js"); 
-//console.log("Review model loaded as:", Review);
+const listings = require("./routes/listing.js");
+const reviews =require("./routes/review.js");  
 
 
 main().then(() =>{
@@ -34,140 +31,9 @@ app.get("/" ,(req,res) =>{
     res.send(" Hi , I am root"); 
 }); 
 
-// Listing Schema Validation middleware using Joi  
-const validateListing =(req,res,next) =>{
-    let {error}= listingSchema.validate(req.body); 
-   if(error){
-    let errMsg = error.details.map((el) => el.message).join(","); 
-    throw new ExpressError(400,errMsg); 
-   }else {
-    next(); 
-   }
+app.use("/listings",listings);
+app.use("/listings/:id/reviews",reviews); 
 
-}
-
-// Review Schema Validation middleware using Joi  
-const validateReview =(req,res,next) =>{
-    let {error}= reviewSchema.validate(req.body); 
-   if(error){
-    let errMsg = error.details.map((el) => el.message).join(","); 
-    throw new ExpressError(400,errMsg); 
-   }else {
-    next(); 
-   }
-
-}
-
-
-// Index Route 
-app.get("/listings" , async (req,res) => {
-    const allListings = await Listing.find({}); 
-    res.render("listings/index.ejs",{allListings}); 
-
-}); 
-// New Route 
-app.get("/listings/new",(req,res) => {
-    res.render("listings/new.ejs"); 
-}) ; 
-// isko show route se upper hai kyuki , db listings/new ka id match karne lagega
-
-
-// Show Route 
-app.get("/listings/:id", async (req,res) => {
-    let {id} = req.params; 
-    const listing = await Listing.findById(id).populate("reviews"); 
-    res.render("listings/show.ejs", {listing}); 
-})
-// CReate Route 
-app.post("/listings" ,validateListing, wrapAsync(async(req,res,next) =>{
-    // if(!req.body.listing){
-    //     throw new ExpressError(400,"Send valid data for listing")
-    // }
-    // we will use joi validation 
-   
-    
-        const newListing = new Listing(req.body.listing); 
-        await newListing.save(); 
-   
-        res.redirect("/listings"); 
-
-   
-  
-})); 
-
-// Edit Route 
-app.get("/listings/:id/edit",wrapAsync(async(req,res) =>{
-    let {id} = req.params; 
-    const listing = await Listing.findById(id); 
-    res.render("listings/edit.ejs",{listing}); 
-
-}));
-
-// Update Route 
-app.put("/listings/:id",validateListing, wrapAsync(async(req,res) =>{
-    if(!req.body.listing){
-        throw new ExpressError(400,"Send valid data for listing")
-    }
-    let {id} = req.params; 
-   await Listing.findByIdAndUpdate(id,{...req.body.listing}); 
-   res.redirect(`/listings/${id}`); 
-}));
-// Delete Route 
-app.delete("/listings/:id",wrapAsync(async(req,res) =>{
-    let {id} = req.params; 
-   let deletedListing = await Listing.findByIdAndDelete(id); 
-   console.log(deletedListing); 
-   res.redirect("/listings");
-
-})); 
-
-// Reviews //post ruoute 
-app.post("/listings/:id/reviews" ,validateReview,wrapAsync(async(req,res) =>{
-    let listing = await Listing.findById(req.params.id); 
-    let newReview = new Review(req.body.review); 
-
-    listing.reviews.push(newReview); 
-
-    await newReview.save(); 
-    await listing.save(); 
-
-    res.redirect(`/listings/${listing._id}`); 
-
-})); 
-
-// Post route for deleting review 
-app.delete("/listings/:id/reviews/:reviewId",wrapAsync(async (req,res) =>{
-    let {id, reviewId} = req.params; 
-
-    // we will use pull operator to delete that review from reviws array of that listing 
-
-    await Listing.findByIdAndUpdate(id ,{$pull: {reviews:reviewId}}); 
-    await Review.findByIdAndDelete(reviewId); 
-
-    res.redirect(`/listings/${id}`); 
-})) 
-
-
-
-// app.get("/listings/new",(req,res) => {
-//     res.render("listings/new.ejs"); 
-// })
-
-// app.get("/testListings",async (req,res) =>{
-//     let sampleListing = new Listing({
-//         title : "my Villa",
-//         description : " By the beach ", 
-//         price : 1200,
-//         location :"goa",
-//         country : "India"
-//     }); 
-//     await sampleListing.save(); 
-//     console.log("Sample was saved "); 
-//     res.send("Successful testing"); 
-
-
-
-// })
 
 // Error middleWares 
 // app.all("/*",(req,res,next) =>{
